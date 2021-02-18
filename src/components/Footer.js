3eedf2797fe3dea7a74aba37c0a36c2975b9371d
alexis andermatten <alexisaandermatten@gmail.com>
2021-02-18 17:13:12 -0300

    cambios en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Footer( { useStyles }) {
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    padding: '40px',
+    marginTop: '2%',
+    backgroundColor: '#29343E',
+  },
+  img:{}   
+}));
+
+
+export default function Footer() {
 
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div>
       <CssBaseline />
       <footer className={classes.footer}>
         <Container maxWidth="sm">
-         <p>Holaaa</p>
+          <img src="/assets/img/footerFinanzas.png"/>
         </Container>
       </footer>
     </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Tabla from '../components/Tabla';
 import { makeStyles } from '@material-ui/core/styles';
+import Footer from '../components/Footer';
 
 export const Home = () => {
 
     const columns = [
-        { id: 'fecha', label: 'Fecha', minWidth: 50, align: 'center'},
-        { id: 'numero', label: 'Número', minWidth: 50, align: 'center'},
-        { id: 'necesidad', label: 'Necesidad', minWidth: 50, align: 'center'},
-        { id: 'solicitante', label: 'Solicitante', minWidth: 50, align: 'center'},
+        { id: 'fecha', label: 'Fecha', minWidth: 50, align: 'center' },
+        { id: 'numero', label: 'Número', minWidth: 50, align: 'center' },
+        { id: 'necesidad', label: 'Necesidad', minWidth: 50, align: 'center' },
+        { id: 'solicitante', label: 'Solicitante', minWidth: 50, align: 'center' },
         { id: 'autorizante', label: 'Autorizante', minWidth: 50, align: 'center' },
         { id: 'area', label: 'Area', minWidth: 50, align: 'center' },
         { id: 'estado', label: 'ESTADO', minWidth: 50, align: 'center' },
@@ -17,72 +18,63 @@ export const Home = () => {
         { id: 'eDoc', label: 'eDOC', minWidth: 20, align: 'center' },
         { id: 'eExp', label: 'eEXP', minWidth: 20, align: 'center' },
     ];
-    
+
     function createData(fecha, numero, necesidad, solicitante, autorizante, area, estado, detalle, req, eDoc, eExp) {
-        return {fecha, numero, necesidad, solicitante, autorizante, area, estado, detalle, req, eDoc, eExp};
+        return { fecha, numero, necesidad, solicitante, autorizante, area, estado, detalle, req, eDoc, eExp };
     }
-    
+
     const rows = [
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General' ,'cancelado', 'ver', 'ver', 'ver', 'ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
         createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
         createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
-        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente',  'ver', 'ver', 'ver', 'ver'),
-        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado',  'ver', 'ver', 'ver','ver'),
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General' , 'cancelado', 'ver', 'ver', 'ver', 'ver'),
-        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente',  'ver', 'ver', 'ver', 'ver'),
-        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica','pendiente', 'ver', 'ver', 'ver', 'ver'),
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General' , 'cancelado','ver', 'ver', 'ver', 'ver'),
-        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente',  'ver', 'ver', 'ver', 'ver'),
-        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver','ver'),
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General' , 'finalizado', 'ver', 'ver', 'ver', 'ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
         createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
         createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
-        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi' , 'Contaduría General' , 'cancelado', 'ver', 'ver', 'ver', 'ver'),
-        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente',  'ver', 'ver', 'ver', 'ver'),
-        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver','ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
+        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
+        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
+        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
+        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
+        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
+        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
+        createData('28/12/2020', 437, '6/1/2021', 'Drazile Mabel', 'Daniel Rodolfi', 'Contaduría General', 'cancelado', 'ver', 'ver', 'ver', 'ver'),
+        createData('26/12/2020', 435, '8/1/2021', 'Lelli Alejandra', 'Monica Zornberg', 'Secretaría Administración', 'pendiente', 'ver', 'ver', 'ver', 'ver'),
+        createData('23/12/2020', 431, '13/1/2021', 'Lelli Alejandra', 'Santiago De La Rosa', 'Secretaría Legal & Técnica', 'finalizado', 'ver', 'ver', 'ver', 'ver'),
     ];
-    
-    const useStyles = makeStyles((theme)=>({
+
+    const useStyles = makeStyles((theme) => ({
         root: {
             width: '100%',
-            
+
         },
         container: {
             maxHeight: 440,
-            
+
         },
-        table:{
+        table: {
             backgroundColor: '#424242',
             color: 'white',
         },
-        roott: {
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100vh',
-          },
-          footer: {
-            padding: theme.spacing(3, 2),
-            marginTop: 'auto',
-            backgroundColor:
-              theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-          },
-          formControl: {
+        
+        formControl: {
             margin: theme.spacing(1),
             minWidth: 120,
-          },
-          selectEmpty: {
+        },
+        selectEmpty: {
             marginTop: theme.spacing(2),
-          },
+        },
     }));
 
     return (
         <>
-            <Tabla 
-                useStyles =  { useStyles } 
-                rows = { rows }
-                columns = { columns }
+            <Tabla
+                useStyles={useStyles}
+                rows={rows}
+                columns={columns}
             />
+            <Footer/>
         </>
     )
 }
